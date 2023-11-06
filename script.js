229cfb25b28698ac6fec1d74110ce5aa4bcb6e61
Leticia Candido <leticiacandidoarq@gmail.com>
2023-11-06 20:17:16 -0300

    funcionalidade setas slider

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let listaSetaDireita = document.getElementsByClassName('seta');
+
+for (let index = 0; index < listaSetaDireita.length; index++) {
+    listaSetaDireita[index].addEventListener('click', function(){
+        let slider;
+        if (this.classList.contains('experiencia')) {
+            
+            slider = document.getElementsByClassName('slider experiencia')[0];
+        } else if (this.classList.contains('cursos')) {
+            
+            slider = document.getElementsByClassName('slider cursos')[0];
+        }
+        if (slider) {
+            if(this.classList.contains('bi-arrow-right-circle-fill')){
+                slider.scrollLeft += 100; 
+            } else if(this.classList.contains('bi-arrow-left-circle-fill')){
+                slider.scrollLeft -= 100;
+            }
+            
+        }
+    })    
+}
+
